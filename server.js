b674fe87d1f44da94c7e46f1df704603dad2742d
Paulo Sergio <Paulo Sergio>
2016-03-07 23:36:58 -0300

    copying files

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
-var inputDir = "input";
+var inputDir = "input2";
 var outputDir = "output";
 var file = "";
 
 var newFile = "";
 newFile = "t.txt";
+
+//TODO solve the sync problem
+function sleep(time, callback) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+    callback();
+}
 
 function renameFile(inputDir,outputDir, onFileContent, onError) {
   fs.readdir(inputDir, function(err, filenames) {
@@ -14,10 +23,18 @@ function renameFile(inputDir,outputDir, onFileContent, onError) {
     }
 
     filenames.forEach(function(filename) {
+
+    	//TODO colocar em uma funcao
+//20131102_181816.jpg
+var res = filename.split('.');
+console.log('extension:' + res[1]);
+console.log(filename);
+console.log("newFile:" + filename.substring(0,4) + "-" +filename.substring(4,6) + "-" + filename.substring(6,8) + ' ' + filename.substring(9,11) + '.' + filename.substring(11,13) + '.' + filename.substring(13,15)+ '.' + res[1]);
+newFile = filename.substring(0,4) + "-" +filename.substring(4,6) + "-" + filename.substring(6,8) + ' ' + filename.substring(9,11) + '.' + filename.substring(11,13) + '.' + filename.substring(13,15)+ '.' + res[1];
+
+//  	2016-03-06 21.44.48
 		file=inputDir + filename;
 		newFile = outputDir + newFile;
-		console.log(file);
-		console.log(newFile);
       fs.readFile(file, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
@@ -25,6 +42,10 @@ function renameFile(inputDir,outputDir, onFileContent, onError) {
         }
 
         fs.createReadStream(file).pipe(fs.createWriteStream(newFile));
+sleep(1000, function() {
+   // executes after one second, and blocks the thread
+});
+
        /*  fs.rename(file, outputDir + file + '2.txt', function(err) {
             if ( err ) console.log('ERROR: ' + err);
         });
