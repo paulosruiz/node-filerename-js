ff5440eaf24e708f25caf5ef5604184f217b1aaf
Paulo Sergio <Paulo Sergio>
2016-03-07 22:42:06 -0300

    copying files

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const fs = require('fs');
 var inputDir = "input";
-var outputDir = "input";
+var outputDir = "output";
 var file = "";
+
+var newFile = "";
+newFile = "t.txt";
+
 function renameFile(inputDir,outputDir, onFileContent, onError) {
   fs.readdir(inputDir, function(err, filenames) {
     if (err) {
       onError(err);
       return;
     }
+
     filenames.forEach(function(filename) {
-file=inputDir + filename;
+		file=inputDir + filename;
+		newFile = outputDir + newFile;
+		console.log(file);
+		console.log(newFile);
       fs.readFile(file, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
           return;
         }
-         fs.rename(file, outputDir + file + '2.txt', function(err) {
+
+        fs.createReadStream(file).pipe(fs.createWriteStream(newFile));
+       /*  fs.rename(file, outputDir + file + '2.txt', function(err) {
             if ( err ) console.log('ERROR: ' + err);
         });
+        */
       });
     });
   });
